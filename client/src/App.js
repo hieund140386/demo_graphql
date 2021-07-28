2d76d5fc44dd236ac85da09461f48282d769d3a9
hieunguyen <hieunguyen.ndh.86@gmail.com>
2021-07-28 23:20:04 +0700

    Modified baseUrl according to development environment

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,10 @@ import { Container } from 'react-bootstrap'
 import { BookList } from './components/BookList'
 import { Forms } from './components/Forms' 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { baseUrl } from './configs/api'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: `${baseUrl}`,
   cache: new InMemoryCache()
 })
 function App() {
---
+++ client/src/configs/api.js
@@ -0,0 +1,1 @@
+export const baseUrl = process.env.NODE_ENV !== 'production' ? `http://localhost:4000/graphql` : `https://aqueous-anchorage-42070.herokuapp.com/graphql`;
